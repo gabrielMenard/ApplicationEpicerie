f603bf574ab15b43ec5a2dd43271b835b740ebef
blob56 <gmenard65@gmail.com>
2021-04-12 14:59:49 -0400

    removed X button in profile and historique

--- devoir4/app/src/main/java/com/example/devoir4/ResetPassword.java
+++ devoir4/app/src/main/java/com/example/devoir4/ResetPassword.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ResetPassword extends AppCompatActivity {
 
     Button envoie;
+    ImageView fermer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +20,22 @@ public class ResetPassword extends AppCompatActivity {
 
         envoie = findViewById(R.id.envoie);
         envoie.setOnClickListener(envoieListener);
+
+        fermer = findViewById(R.id.x_button);
+        fermer.setOnClickListener(fermerListener);
     }
 
     View.OnClickListener envoieListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             startActivity(new Intent(ResetPassword.this, CreerCompte.class));
+        }
+    };
+
+    View.OnClickListener fermerListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(ResetPassword.this, Preauthentification.class));
         }
     };
 }
